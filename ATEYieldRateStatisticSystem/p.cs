f2e8d60471725b2964528fc47d10fb55048900c6
Edward <edward_song@yeah.net>
2017-07-03 14:01:25 +0800

    add STAGE

--- ATEYieldRateStatisticSystem/p.cs
+++ ATEYieldRateStatisticSystem/p.cs
@@ -36,8 +36,138 @@ namespace ATEYieldRateStatisticSystem
         public static string SFCS722Webservice = @"http://10.62.201.77/Tester.WebService/WebService.asmx"; //default
         public static string TEST721Webservice = @"http://172.0.1.172/Tester.WebService/WebService.asmx"; //default
         public static string TEST722Webservice = @"http://172.0.1.171/Tester.WebService/WebService.asmx"; //default
+        //public static List<string> ATEBeforStage = new List<string>();
+        public static STAGE ATEStage;
+        //public static List<string> ATEAfterStage = new List<string>();
+        public static List<STAGE> ATEBeforeStage = new List<STAGE>();
+        public static List<STAGE> ATEAfterStage = new List<STAGE>();
+        public static List<STAGE> ATEOtherStage = new List<STAGE>();
+        
+        #endregion
+
+
+        #region StageList
+
+            //AA
+           public  static STAGE AA = new STAGE("AA");          
+            //AO
+           public static STAGE AO = new STAGE("AO");
+            //AB
+           public static STAGE AB = new STAGE("AB");     
+            //AC
+           public static STAGE AC = new STAGE("AC");      
+            //AD
+           public static STAGE AD = new STAGE("AD"); 
+            //AG
+           public static STAGE AG = new STAGE("AG");        
+            //AL
+           public static STAGE AL = new STAGE("AL");
+           //AY
+           public static STAGE AY = new STAGE("AY");
+           //AZ
+           public static STAGE AZ = new STAGE("AZ");
+           //CJ
+           public static STAGE CJ = new STAGE("CJ");
+           //CS
+           public static STAGE CS = new STAGE("CS");
+           //EA
+           public static STAGE EA = new STAGE("EA");
+           //FA
+           public static STAGE FA = new STAGE("FA");
+           //IB
+           public static STAGE IB = new STAGE("IB");
+           //IC
+           public static STAGE IC = new STAGE("IC");
+           //ID
+           public static STAGE ID = new STAGE("ID");
+           //IE
+           public static STAGE IE = new STAGE("IE");
+           //IF
+           public static STAGE IF = new STAGE("IF");
+           //IG
+           public static STAGE IG = new STAGE("IG");
+           //PA
+           public static STAGE PA = new STAGE("PA");
+        //QA
+           public static STAGE QA = new STAGE("QA");
+           //QB 
+           public static STAGE QB = new STAGE("QB");
+           //QC
+           public static STAGE QC = new STAGE("QC");
+           //QD
+           public static STAGE QD = new STAGE("QD");
+           //QE
+           public static STAGE QE = new STAGE("QE");
+           //QF
+           public static STAGE QF = new STAGE("QF");
+           //QG
+           public static STAGE QG = new STAGE("QG");
+           //QH
+           public static STAGE QH = new STAGE("QH");
+           //RA
+           public static STAGE RA = new STAGE("RA");
+           //RC
+           public static STAGE RC = new STAGE("RC");
+           //RD
+           public static STAGE RD = new STAGE("RD");
+           //RE
+           public static STAGE RE = new STAGE("RE");
+           //RF
+           public static STAGE RF = new STAGE("RF");
+           //RG
+           public static STAGE RG = new STAGE("RG");
+           //RH
+           public static STAGE RH = new STAGE("RH");
+           //RI
+           public static STAGE RI = new STAGE("RI");
+           //RJ
+           public static STAGE RJ = new STAGE("RJ");
+           //RK
+           public static STAGE RK = new STAGE("RK");
+           //SA
+           public static STAGE SA = new STAGE("SA");
+           //SB
+           public static STAGE SB = new STAGE("SB");
+           //ST
+           public static STAGE ST = new STAGE("ST");
+           //TA
+           public static STAGE TA = new STAGE("TA");
+           //TB
+           public static STAGE TB = new STAGE("TB");
+           //TC
+           public static STAGE TC = new STAGE("TC");
+           //TD
+           public static STAGE TD = new STAGE("TD");
+           //TE
+           public static STAGE TE = new STAGE("TE");
+           //TF
+           public static STAGE TF = new STAGE("TF");
+           //TG
+           public static STAGE TG = new STAGE("TG");
+           //TH
+           public static STAGE TH = new STAGE("TH");
+           //TI
+           public static STAGE TI = new STAGE("TI");
+           //TJ
+           public static STAGE TJ = new STAGE("TJ");
+           //TK
+           public static STAGE TK = new STAGE("TK");
+           //TL
+           public static STAGE TL = new STAGE("TL");
+           //TY
+           public static STAGE TY = new STAGE("TY");
+           //ZL
+           public static STAGE ZL = new STAGE("ZL");
+           //ZM
+           public static STAGE ZM = new STAGE("ZM");
+
+
+        
 
         #endregion
+
+
+
 
         #region enum
 
@@ -202,13 +332,194 @@ namespace ATEYieldRateStatisticSystem
             while (DateTime.Now.Ticks - time.Ticks < span)
             {
                 Application.DoEvents();
-            }
-
+            }     
         }
 
         #endregion
 
+        /// <summary>
+        /// 
+        /// </summary>
+        private void InitATEStageInfo()
+        {
+            //AA
+           AA.StageValue = @"SMT_input(BOT)";
+            //AO   
+           AO.StageValue = @"INPUT";
+            //AB  
+           AB.StageValue = @"System Auto Assign";
+            //AC
+           AC.StageValue = @"Swap";
+            //AD
+           AD.StageValue = @"DIP_Touch_Up";
+            //AG
+           AG.StageValue = @"SMT_TOP";
+            //AL
+           AL.StageValue = @"PC CHANGE MO STAGE";
+            //AY
+           AY.StageValue = @"SCTSTNCL top online";
+            //AZ
+           AZ.StageValue = @"SCTSTNCL bot online";
+            //CJ
+           AZ.StageValue = @"TLINE TE STAGE";
+            //CS
+          CS.StageValue = @"CHECK SAMPLE TEST";
+            //EA
+          EA.StageValue = @"REWORK";
+            //FA
+          FA.StageValue = @"X_ray";
+            //IB
+          IB.StageValue = @"DIP_VI";
+            //IC
+          IC.StageValue = @"DIP_FINAL";
+            //ID
+          ID.StageValue = @"SMT_RB";
+            //IE
+          IE.StageValue = @"SMT_VIB";
+            //IF
+          IF.StageValue = @"SMT_RT";
+            //IG
+          IG.StageValue = @"SMT_VIT";
+            //PA
+          PA.StageValue = @"PACKING";
+            //QA
+          QA.StageValue = @"AQPP";
+            //QB
+          QB.StageValue = @"PQC";
+            //QC
+          QC.StageValue = @"OOB";
+            //QD
+          QD.StageValue = @"F/A OOB";
+            //QE
+          QE.StageValue = @"F/B OOB";
+            //QF
+          QF.StageValue = @"Solder A FQC";
+            //QG
+          QG.StageValue = @"Solder B FQC";
+            //QH
+          QH.StageValue = @"QC Force";
+            //RA
+          RA.StageValue = @"SMT Sorting";
+            //RC
+          RC.StageValue = "DIP Sorting";
+            //RD
+          RD.StageValue = @"On-line Repair";
+            //RE
+          RE.StageValue = @"Repair QC";
+            //RF
+          RF.StageValue = @"BGA";
+            //RG
+          RG.StageValue = @"Feedback";
+            //RH
+          RH.StageValue = @"Off-line Repair";
+            //RI
+          RI.StageValue = @"Clean MB";
+            //RJ
+          RJ.StageValue = @"DOA Repair";
+            //RK
+          RK.StageValue = @"FA DOA CHECKIN";
+            //SA
+          SA.StageValue = @"STOREIN";
+            //SB
+          SB.StageValue = @"SPI BOT";
+            //ST
+          ST.StageValue = @"SPI TOP";
+            //TA
+          TA.StageValue = @"DIP_ATE";
+            //TB
+          TB.StageValue = @"SMT_ICT";
+            //TC
+          TC.StageValue = @"DIP_ICT";
+            //TD
+          TD.StageValue = @"DIP_Function_A";
+            //TE
+          TE.StageValue = @"DIP_Function_B";
+            //TF
+          TF.StageValue = @"DIP_Function_L3";
+            //TG
+          TG.StageValue = @"DIP_Function_CSD";
+            //TH
+          TH.StageValue = @"DIP_Function_CSD";
+            //TI
+          TI.StageValue = @"READ FOR AOIA FILE";
+            //TJ
+          TJ.StageValue = @"READ FOR AOIB FILE";
+            //TK
+          TK.StageValue = @"AOIB(AOI B Side)";
+            //TL
+          TL.StageValue = @"AOIA(AOI A Side)";
+            //TY
+          TY.StageValue = @"DIP_Function_CSD";
+            //ZL
+          ZL.StageValue = @"MDMS";
+            //ZM
+          ZM.StageValue = @"Already Store-in";
 
+            //
+          ATEBeforeStage.Add(AA);
+          ATEBeforeStage.Add(ID);
+          ATEBeforeStage.Add(TL);
+          ATEBeforeStage.Add(AG);
+          ATEBeforeStage.Add(IF);
+          ATEBeforeStage.Add(TK);
+          ATEBeforeStage.Add(IB);
+          //
+          ATEStage = TA;
+          //
+          ATEAfterStage.Add(TD);
+          ATEAfterStage.Add(IC);
+          ATEAfterStage.Add(QD);
+          ATEAfterStage.Add(PA);
+
+            //
+           ATEOtherStage.Add(AO);
+           ATEOtherStage.Add(AB);
+           ATEOtherStage.Add(AC);
+           ATEOtherStage.Add(AD);
+           ATEOtherStage.Add(AL);
+           ATEOtherStage.Add(AY);
+           ATEOtherStage.Add(AZ);
+           ATEOtherStage.Add(CJ);
+           ATEOtherStage.Add(CS);
+           ATEOtherStage.Add(EA);
+           ATEOtherStage.Add(FA);
+           ATEOtherStage.Add(IE);
+           ATEOtherStage.Add(IF);
+           ATEOtherStage.Add(IG);
+           ATEOtherStage.Add(PA);
+           ATEOtherStage.Add(QA);
+           ATEOtherStage.Add(QB);
+           ATEOtherStage.Add(QC);
+           ATEOtherStage.Add(QE);
+           ATEOtherStage.Add(QF);
+           ATEOtherStage.Add(QG);
+           ATEOtherStage.Add(QH);
+           ATEOtherStage.Add(RC);
+           ATEOtherStage.Add(RD);
+           ATEOtherStage.Add(RE);
+           ATEOtherStage.Add(RF);
+           ATEOtherStage.Add(RG);
+           ATEOtherStage.Add(RH);
+           ATEOtherStage.Add(RI);
+           ATEOtherStage.Add(RJ);
+           ATEOtherStage.Add(RK);
+           ATEOtherStage.Add(SA);
+           ATEOtherStage.Add(SB);
+           ATEOtherStage.Add(ST);
+           ATEOtherStage.Add(TA);
+           ATEOtherStage.Add(TB);
+           ATEOtherStage.Add(TC);
+           ATEOtherStage.Add(TE);
+           ATEOtherStage.Add(TF);
+           ATEOtherStage.Add(TG);
+           ATEOtherStage.Add(TH);
+           ATEOtherStage.Add(TI);
+           ATEOtherStage.Add(TJ);
+           ATEOtherStage.Add(TY);
+           ATEOtherStage.Add(ZL);
+           ATEOtherStage.Add(ZM);
+        }
+        
 
         private static  string PublicResourceFileName = Application.ProductName + ".Resources";
         /// <summary>
@@ -279,5 +590,18 @@ namespace ATEYieldRateStatisticSystem
         }
 
 
+
+
+        public class STAGE
+        {
+            //private readonly string _name = "";
+            public string StageName { set; get; }
+            public string StageValue { set; get; }
+
+            public STAGE(string stagename)
+            {
+                StageName = stagename;
+            }
+        }
     }
 }
